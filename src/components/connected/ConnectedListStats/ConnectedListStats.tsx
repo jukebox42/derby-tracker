32de89fa6f3bb12d3ad23481a1e1c9b539a82bf7
john <jukebox42@gmail.com>
2024-03-03 19:44:14 -0500

    stuff

--- src/components/connected/ConnectedListStats/ConnectedListStats.tsx
+++ src/components/connected/ConnectedListStats/ConnectedListStats.tsx
@@ -5,7 +5,7 @@ import { UseConnectedList } from "@/hooks/useConnectedList";
 
 export type ListStatsProps<T> = {
   stats: ListStatProps[],
-} & UseConnectedList<T>;
+} & UseConnectedList<T, any>;
 
 export const ConnectedListStats = <T,>({ stats, isLoading }: ListStatsProps<T>) => (
   <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
